809e01b3562a3812b91511ca8bfb4df575dddd18
m3lt3m <mltm.prmksz@gmail.com>
2010-01-13 20:54:10 +0000

    duzenleme

--- ikiuzeri.sh
+++ ikiuzeri.sh
@@ -9,35 +9,24 @@ usage(){
 }
 
 if [ "$#" -lt 2 ]; then { usage;  exit 1; } fi
+[[ -d "$1" || -d "$2" ]] || { echo -e "Error: $source directory not exist"; exit 1; }
 
-
-#[ -d "$1" ] || { echo -e "Error: $source directory not exist"; exit 1; }
-#[ -d "$2" ] || { echo -e "Error: $destination directory not exist"; exit 1; }
-#if [[ -t "$fd" || -S /dev/stdin ]]
-
-source="$1"
-destination="$2"
 component=""
 
 if [ "$3" != "all" ]; then
     component=`echo "$3" | sed -e 's/\./\//' `
 fi
-#[ -d "$source/devel/$component"  ]  || { echo -e " $component not found component "; exit 1; }
+[ -d "$1/devel/$component"  ]  || { echo -e " $component not found component "; exit 1; }
 
-
+source="$1"
+destination="$2"
 #cp -R  $source/* $destination/ && 
 
-#source= echo $1 | sed -e 's/\//\\\//g'
-#destination= echo $2 | sed -e 's/\//\\\//g'
-
-
-
+# devel eklenmeli ikiuzerialti.sh 'a
 echo "bilesen   $component source ${source//\//\\/}  destination ${destination//\//\\/}  "
+sed -e 's/\$source/'${source//\//\\/}'/' -e 's/\$destination/'${destination//\//\\/}'/' ikiuzerialti.sh  |  grep $component |  awk -F"\n" '{  system($1); } '
 
-sed -e 's/source/'${source//\//\\/}'/' -e 's/destination/'${destination//\//\\/}'/' ikiuzerialti.sh  |  grep $component |  awk -F"\n" '{  system($1); } '
 
-
-#cat ikiuzerialti.sh | grep $component | sed -e  's/$source/'$source'/' -e 's/$destination/'$destination'/'  |  awk -F"\n" '{  system($1); } '
 
 
 
