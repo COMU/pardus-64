8d2c3e8e5155a3808f7372bf86578430afb2e1d5
m3lt3m <mltm.prmksz@gmail.com>
2010-01-13 19:29:45 +0000

    string replace yapıldı

--- ikiuzeri.sh
+++ ikiuzeri.sh
@@ -13,7 +13,7 @@ if [ "$#" -lt 2 ]; then { usage;  exit 1; } fi
 
 #[ -d "$1" ] || { echo -e "Error: $source directory not exist"; exit 1; }
 #[ -d "$2" ] || { echo -e "Error: $destination directory not exist"; exit 1; }
-
+#if [[ -t "$fd" || -S /dev/stdin ]]
 
 source="$1"
 destination="$2"
@@ -22,16 +22,19 @@ component=""
 if [ "$3" != "all" ]; then
     component=`echo "$3" | sed -e 's/\./\//' `
 fi
+#[ -d "$source/devel/$component"  ]  || { echo -e " $component not found component "; exit 1; }
 
 
-[ -d "$source/devel/$component"  ]  || { echo -e " $component not found component "; exit 1; }
+#cp -R  $source/* $destination/ && 
+
+#source= echo $1 | sed -e 's/\//\\\//g'
+#destination= echo $2 | sed -e 's/\//\\\//g'
 
 
-echo "bilesen   $component source $source destination $destination "
 
-cp -R  $source/* $destination/ && 
+echo "bilesen   $component source ${source//\//\\/}  destination ${destination//\//\\/}  "
 
-sed -e 's/source/'$source'/' -e 's/destination/'$destination'/' ikiuzerialti.sh  |  grep $component |  awk -F"\n" '{  system($1); } '
+sed -e 's/source/'${source//\//\\/}'/' -e 's/destination/'${destination//\//\\/}'/' ikiuzerialti.sh  |  grep $component |  awk -F"\n" '{  system($1); } '
 
 
 #cat ikiuzerialti.sh | grep $component | sed -e  's/$source/'$source'/' -e 's/$destination/'$destination'/'  |  awk -F"\n" '{  system($1); } '
